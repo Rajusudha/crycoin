6232ea790bbfd8975b03ebd5beb23e30eeb3f308
Raju Kumar <rajukumar20182018@gmail.com>
2024-11-09 12:10:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import CoinPage from './pages/CoinPage';
+import Compare from './pages/Compare';
+import WatchListPage from './pages/WatchListPage';
+import {createContext} from 'react'
 
+export const themeContext=createContext(null)
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <Router>
+        <Routes>
+          <Route path='/' element={[<Home id="light" />]} />
+          <Route path='/dashboard' element={[<Dashboard />]}/>
+          <Route path='/coin/:id' element={[<CoinPage />]}/>
+          <Route path='/compare' element={[<Compare />]}/>
+          <Route path='/watchList' element={[<WatchListPage/>]}/>
+        </Routes>
+      </Router>
+     
+
     </div>
   );
 }
+
 
 export default App;
